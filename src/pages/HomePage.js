0b464b4ceb1f134c97ceb96de628cab0e061cbb6
co1ne <huangwei@runsdata.com>
2018-07-02 18:12:18 +0800

    init push

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import HomePageLayout from '../components/HomePageLayout'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <HomePageLayout/>
+        );
+    }
+}
+
+export default HomePage;
