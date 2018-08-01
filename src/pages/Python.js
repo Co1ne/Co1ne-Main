7dd84ad309fe703e3625e90d023a5f6973831094
Co1ne <793585999@qq.com>
2018-08-01 21:08:56 +0800

    add page

---
+++ src/pages/Python.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+class Python extends Component {
+    render() {
+        return (
+            <p>Hello Python</p>
+        );
+    }
+}
+
+export default Python;
