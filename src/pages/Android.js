728dd46a7b70fcc866acc04d7f39e43d5be952d9
Co1ne <793585999@qq.com>
2018-08-07 14:22:38 +0800

    add menu click event

---
+++ src/pages/Android.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Android extends Component {
+    render() {
+        return (
+           <p>Hello,World</p>
+        );
+    }
+}
+
+export default Android;
